ae7159b2d342eac582eee07e14a13077460e760e
Maltemork <malte.sorensen@protonmail.com>
2024-03-11 21:48:29 +0100

    Made navbar reactive to location

--- src/NavHeader.tsx
+++ src/NavHeader.tsx
@@ -1,40 +1,55 @@
 // TODO - IMPLEMENT SECURITY.
 // import { useAuth } from "./security/AuthProvider";
 // import AuthStatus from "./security/AuthStatus";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import "./NavHeader.css";
 import CameraIcon from "@mui/icons-material/Camera";
 import LocalMoviesIcon from "@mui/icons-material/LocalMovies";
-import { LocalActivity, AccountCircle } from "@mui/icons-material";
+import { LocalActivity, AccountCircle, LocationOff } from "@mui/icons-material";
 
 export default function NavHeader() {
+  const location = useLocation();
   // const auth = useAuth();
   return (
     <nav className="nav-header">
       <ul className="nav-header-ul">
-        <li>
+        <li
+          id="nav-header-logo"
+          className={location.pathname == "/" ? "active-header" : ""}
+        >
           <NavLink to="/">
             <CameraIcon />
-            <p>Forside</p>
           </NavLink>
         </li>
-        <li id="nav-film">
+        <li
+          id="nav-film"
+          className={location.pathname == "/film" ? "active-header" : ""}
+        >
           <NavLink to="/film">
             <LocalMoviesIcon />
             <p>Film</p>
           </NavLink>
         </li>
-        <li>
+        <li
+          className={location.pathname == "/biografer" ? "active-header" : ""}
+        >
           <NavLink to="/biografer">
             <LocalActivity />
             <p>Biografer</p>
           </NavLink>
         </li>
 
-        <li id="nav-signin">
+        <li
+          id="nav-signin"
+          className={
+            location.pathname == "/log-ind" ||
+            location.pathname == "/opret-konto"
+              ? "active-header"
+              : ""
+          }
+        >
           <NavLink to="/log-ind">
             <AccountCircle />
-            <p>Opret Konto | Log Ind</p>
           </NavLink>
         </li>
       </ul>
