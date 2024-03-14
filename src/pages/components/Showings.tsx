e12f39a1a88bff131612c30537b23b0907333046
fili0727 <123998888+fili0727@users.noreply.github.com>
2024-03-14 13:52:04 +0100

    Create Showings.tsx

---
+++ src/pages/components/Showings.tsx
@@ -0,0 +1,27 @@
+// import "../styling/mainpage.css";
+
+// export default function Movie(movieProps: MovieProps) {
+//     const { title, description, poster, showtimes } = movieProps;
+//     return (
+//         <div className="movie">
+//             <img src={poster}/>
+//             <div>
+//                 <h1>{title}</h1>
+//                 <p>{description}</p>
+//                 <div className="time_container">
+//                     {showtimes.map((showtime, index) => (
+//                         <a key={index}>{showtime}</a>
+//                     ))}
+//                 </div>
+//             </div>
+            
+//         </div>
+//     );
+// }
+
+// interface MovieProps {
+//     title: string;
+//     description: string;
+//     poster: string;
+//     showtimes: string[];
+// }
