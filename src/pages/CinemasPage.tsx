e3f942ef50e6c3e4c0414457223aa33ee4c3a3ca
TheDanishMexican <danieljchristjansen@gmail.com>
2024-03-16 17:44:16 +0100

    did the cinema page

--- src/pages/CinemasPage.tsx
+++ src/pages/CinemasPage.tsx
@@ -1,5 +1,15 @@
+import Cinema from '../interfaces/cinema'
+import { useEffect, useState } from 'react'
+import './styling/cinemaspage.css'
+
 export default function CinemasPage() {
     const [cinemas, setCinemas] = useState<Cinema[]>([])
+
+    useEffect(() => {
+        fetch('http://localhost:8080/cinemas')
+            .then((response) => response.json())
+            .then((data) => setCinemas(data))
+    }, [])
 
     return (
         <div>
@@ -8,6 +18,14 @@ export default function CinemasPage() {
                 This page is meant for displaying the different cinemas and
                 picking showings.
             </p>
+            <ul style={{ listStyle: 'none' }}>
+                {cinemas.map((cinema, index) => (
+                    <li className="cinema-card" key={index}>
+                        <h3>{cinema.name}</h3>
+                        <p>{cinema.location}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
