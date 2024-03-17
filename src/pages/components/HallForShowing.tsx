b4fa96929bd31e868d893143ba8dabf43e43edc7
TheDanishMexican <danieljchristjansen@gmail.com>
2024-03-17 15:53:07 +0100

    made selecting multiple seats possible

--- src/pages/components/HallForShowing.tsx
+++ src/pages/components/HallForShowing.tsx
@@ -2,10 +2,14 @@ import Seat from '../../interfaces/seat'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import '../styling/hallforshowing.css'
+import Showing from '../../interfaces/showing'
 
 export default function HallForShowing() {
     const { showingId } = useParams()
     const [seats, setSeats] = useState<Seat[]>([])
+    const [showing, setShowing] = useState<Showing>()
+    const [reservedSeats, setReservedSeats] = useState<Seat[]>([])
+    const [selectedSeats, setSelectedSeats] = useState<Seat[]>([])
 
     useEffect(() => {
         fetch(`http://localhost:8080/showings/${showingId}/seats`)
@@ -13,9 +17,47 @@ export default function HallForShowing() {
             .then((data) => setSeats(data))
     }, [showingId])
 
+    useEffect(() => {
+        fetch(`http://localhost:8080/showings/${showingId}`)
+            .then((response) => response.json())
+            .then((data) => setShowing(data))
+    }, [showingId])
+
+    useEffect(() => {
+        fetch(`http://localhost:8080/showings/${showingId}/reserved_seats`)
+            .then((response) => response.json())
+            .then((data) => setReservedSeats(data))
+    }, [showingId])
+
+    const handleSeatClick = (seat: Seat) => {
+        if (
+            !reservedSeats.some(
+                (reservedSeat) => reservedSeat.seatNumber === seat.seatNumber
+            )
+        ) {
+            if (
+                selectedSeats.some(
+                    (selected) => selected.seatNumber === seat.seatNumber
+                )
+            ) {
+                setSelectedSeats(
+                    selectedSeats.filter(
+                        (selected) => selected.seatNumber !== seat.seatNumber
+                    )
+                )
+            } else {
+                setSelectedSeats([...selectedSeats, seat])
+            }
+        }
+    }
+
     return (
         <div>
             <h1>Seats</h1>
+            <p>
+                Movie: {showing?.movie.name} -- Time: {showing?.startTime} --
+                Date: {showing?.showingDate}
+            </p>
             <h5 style={{ color: 'purple' }}>
                 ---------------------------------Screen
                 here---------------------------------
@@ -23,7 +65,26 @@ export default function HallForShowing() {
             <div className="hall-container">
                 <div className="hall-grid">
                     {seats.map((seat, index) => (
-                        <div className="seat" key={index}>
+                        <div
+                            className={`seat ${
+                                reservedSeats.some(
+                                    (reservedSeat) =>
+                                        reservedSeat.seatNumber ===
+                                        seat.seatNumber
+                                )
+                                    ? 'reserved'
+                                    : ''
+                            } ${
+                                selectedSeats.some(
+                                    (selected) =>
+                                        selected.seatNumber === seat.seatNumber
+                                )
+                                    ? 'selected'
+                                    : ''
+                            }`}
+                            key={index}
+                            onClick={() => handleSeatClick(seat)}
+                        >
                             <p>{seat.seatNumber}</p>
                         </div>
                     ))}
