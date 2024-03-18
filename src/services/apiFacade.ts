0efcffb5cf949980af974117a8c86cf4d7095190
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2024-03-18 09:41:56 +0100

    added token to headers

--- src/services/apiFacade.ts
+++ src/services/apiFacade.ts
@@ -34,7 +34,7 @@ async function addMovie(newMovie: Movie): Promise<Movie> {
     "Content-Type": "application/json",
     // Authorization: `Bearer ${token}`,
   };
-  const options = makeOptions(method, newMovie, headers);
+  const options = makeOptions(method, newMovie, headers, true);
   const URL = newMovie.id ? `${MOVIES_URL}/${newMovie.id}` : MOVIES_URL;
   return fetch(URL, options).then(handleHttpErrors);
 }
@@ -45,7 +45,7 @@ async function deleteMovie(id: number): Promise<Movie> {
     "Content-Type": "application/json",
     // Authorization: `Bearer ${token}`,
   };
-  const options = makeOptions("DELETE", null, headers);
+  const options = makeOptions("DELETE", null, headers, true);
   return fetch(`${MOVIES_URL}/${id}`, options).then(handleHttpErrors);
 }
 
