9ef6531dd55540ed9135b5464db15e1533efd1b7
TheDanishMexican <danieljchristjansen@gmail.com>
2024-03-20 17:46:44 +0100

    fixed username variable

--- src/pages/components/ReservationOverview.tsx
+++ src/pages/components/ReservationOverview.tsx
@@ -41,7 +41,7 @@ export default function ReservationOverview() {
                         hallId,
                         totalPrice: reservationPrice,
                         seatPrice,
-                        userName: username,
+                        username,
                     },
                     undefined,
                     true
