13f45843d52753c1b11d616467d01bb957f8a031
TheDanishMexican <danieljchristjansen@gmail.com>
2024-03-22 09:36:45 +0100

    added search in showings for admin

--- src/pages/AdminShowingsPage.tsx
+++ src/pages/AdminShowingsPage.tsx
@@ -11,6 +11,12 @@ export default function AdminShowingsPage() {
 
     const [openDialog, setOpenDialog] = useState(false)
 
+    const [searchTerm, setSearchTerm] = useState<string>('')
+
+    const filteredShowings = showings.filter((showing) =>
+        showing.movie.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+
     function handleOpenDialog() {
         setOpenDialog(true)
     }
@@ -49,12 +55,25 @@ export default function AdminShowingsPage() {
     return (
         <div>
             <AdminNavbar />
-            <button
-                className="add-showing-button"
-                onClick={() => handleOpenDialog()}
-            >
-                Add Showing
-            </button>
+            <div className="btn-search">
+                <div>
+                    <button
+                        className="add-showing-button"
+                        onClick={() => handleOpenDialog()}
+                    >
+                        Add Showing
+                    </button>
+                </div>
+                <div>
+                    <input
+                        className="search-bar-showings"
+                        type="text"
+                        placeholder="Search by movie name..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                </div>
+            </div>
             <table>
                 <thead>
                     <tr>
@@ -67,7 +86,7 @@ export default function AdminShowingsPage() {
                     </tr>
                 </thead>
                 <tbody>
-                    {showings.map((showing) => (
+                    {filteredShowings.map((showing) => (
                         <tr key={showing.id}>
                             <td>{showing.id}</td>
                             <td>{showing.movie.name}</td>
--- src/pages/components/AddShowingDialog.tsx
+++ src/pages/components/AddShowingDialog.tsx
@@ -2,9 +2,10 @@ import '../styling/addshowingdialog.css'
 
 export default function AddShowingDialog() {
     return (
-        <div>
-            <h1>Add Showing</h1>
+        <div className="add-showing-form">
+            <h1>If you made it this far</h1>
             <p>We tried but did not succeed with this part</p>
+            <p>Big apologies</p>
         </div>
     )
 }
